df3e705e961d6f6128d4d7ad83d858b918fe9ef8
Travis Swicegood <development@domain51.com>
2012-03-24 16:29:32 -0500

    Swap out to a basic detail view

--- atxpy_home/views.py
+++ atxpy_home/views.py
@@ -1,5 +1,20 @@
-from django.views.generic import TemplateView
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404
+from django.views.generic import DetailView
+
+from hero_content.models import Hero
 
 
-class IndexView(TemplateView):
+class IndexView(DetailView):
+    model = Hero
+    queryset = Hero.objects.all().select_related()
     template_name = "index.html"
+
+    def get_object(self, queryset=None):
+        if queryset is None:
+            queryset = self.get_queryset()
+
+        try:
+            return queryset.all()[0]
+        except IndexError:
+            raise Http404("Unable to load hero content")
