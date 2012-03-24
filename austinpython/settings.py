4fe9e0334b607349bc53cb56257c5c786b0b9bb3
Travis Swicegood <development@domain51.com>
2012-03-24 16:14:19 -0500

    add hero_content to the installed apps

--- austinpython/settings.py
+++ austinpython/settings.py
@@ -130,6 +130,8 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+
+    "hero_content",
 )
 
 # A sample logging configuration. The only tangible logging
