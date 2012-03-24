91b4017ab52fe431ce31279ddea92814a7562be1
Travis Swicegood <development@domain51.com>
2012-03-24 14:15:08 -0500

    remove all original code

--- README.md
+++
@@ -1,8 +0,0 @@
-AustinPython.org
-================
-
-The code running the AustinPython.org website.
-
-It is HIGHLY recommended that you develop against the Vagrant environment:
-
-* https://github.com/awpug/austinpython.org/wiki
--- austinpython/tests.py
+++
@@ -1,8 +0,0 @@
-""" Basic helpers for tests in AustinPython. """
-
-class Mock(object):
-    """ Serves as a sort of object literal for mocking. """
-    def __init__(self, **kwargs):
-        for key, val in kwargs.iteritems():
-            setattr(self, key, val)
-
