e3a435642cf98c446c21976f63567d99129638b7
Travis Swicegood <development@domain51.com>
2012-03-31 15:38:01 -0500

    adjust queryset to only show current one

--- atxpy_home/views.py
+++ atxpy_home/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.http import Http404
 from django.views.generic import DetailView
 
@@ -6,8 +7,13 @@ from hero_content.models import Hero
 
 class IndexView(DetailView):
     model = Hero
-    queryset = Hero.objects.all().select_related()
+    queryset = Hero.objects.all()
     template_name = "index.html"
+
+    def get_queryset(self, queryset=None):
+        if queryset is None:
+            queryset = self.queryset
+        return queryset.filter(pub_date__lte=datetime.now)
 
     def get_object(self, queryset=None):
         if queryset is None:
