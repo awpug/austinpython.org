ee30f30d96db471bf49b62f3011019bc7ce25450
Travis Swicegood <development@domain51.com>
2012-03-31 15:38:11 -0500

    enable markdown

--- austinpython/settings.py
+++ austinpython/settings.py
@@ -124,6 +124,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
+    'django.contrib.markup',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
