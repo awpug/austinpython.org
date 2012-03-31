f448c8917303a6f5dc5773765a3b55efe76a31c9
Travis Swicegood <development@domain51.com>
2012-03-31 15:26:05 -0500

    include a pub_date

--- hero_content/models.py
+++ hero_content/models.py
@@ -4,10 +4,9 @@ from django.db import models
 class Hero(models.Model):
     title = models.CharField(max_length=200)
     summary = models.TextField()
-    body = models.TextField()
+    pub_date = models.DateTimeField()
     action_text = models.CharField(max_length=200, default="RSVP on Meetup")
     action_url = models.URLField()
-    source_id = models.CharField(max_length=250)
 
     def __unicode__(self):
         return self.title
