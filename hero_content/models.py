bdd75ba33dd2b7823840eb08ac36df0158745c23
Travis Swicegood <development@domain51.com>
2012-04-01 17:47:48 -0500

    make these so pep8 doesn't complain

--- hero_content/models.py
+++ hero_content/models.py
@@ -3,13 +3,20 @@ import urllib
 
 
 class Hero(models.Model):
-    title = models.CharField(max_length=200, help_text=u"Shows up at the top")
-    summary = models.TextField(help_text=u"Shows up along side the events tats")
-    pub_date = models.DateTimeField(help_text=u"The date this should show up on the site")
-    action_text = models.CharField(max_length=200, default="RSVP on Meetup", help_text=u"Text of the action button")
-    action_url = models.URLField(help_text=u"URL to send people to when they want to sign up")
-    location = models.CharField(max_length=200, help_text=u"Where is the meeting going to be?")
-    datetime = models.DateTimeField(help_text=u"When does the event happen?")
+    title = models.CharField(max_length=200,
+            help_text=u"Shows up at the top")
+    summary = models.TextField(
+            help_text=u"Shows up along side the events ")
+    pub_date = models.DateTimeField(
+            help_text=u"The date this should show up on the site")
+    action_text = models.CharField(max_length=200, default="RSVP on Meetup",
+            help_text=u"Text of the action button")
+    action_url = models.URLField(
+            help_text=u"URL to send people to when they want to sign up")
+    location = models.CharField(max_length=200,
+            help_text=u"Where is the meeting going to be?")
+    datetime = models.DateTimeField(
+            help_text=u"When does the event happen?")
 
     @property
     def location_map_url(self):
